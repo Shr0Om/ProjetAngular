94719480ce25b850f5daa718de4c737509735b9f
tanguy <tanguy.paillard@epita.fr>
2020-09-09 17:03:08 +0200

    add gitignore for node modules cause its boring

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable ';
+
+
+@Injectable()
+export class httpService{
+    constructor(private htpp: HttpClient) { }
+
+    getListOfPhoto():
+        return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=5547db88b5bfc7ba2b56b8a2395caadd&tags=chien&format=json&nojsoncallback=1');
 
 @Component({
   selector: 'app-api',
@@ -6,9 +16,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api.component.css']
 })
 export class ApiComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
 
