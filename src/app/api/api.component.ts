7c9f92219fb1a8d3de02552cb0e8661f69532903
tanguy <tanguy.paillard@epita.fr>
2020-09-09 17:14:00 +0200

    je modifie pour le gros mechant kevin

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -7,8 +7,6 @@ import { Observable } from 'rxjs/Observable ';
 export class httpService{
     constructor(private htpp: HttpClient) { }
 
-    getListOfPhoto():
-        return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=5547db88b5bfc7ba2b56b8a2395caadd&tags=chien&format=json&nojsoncallback=1');
 
 @Component({
   selector: 'app-api',
