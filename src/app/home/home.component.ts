3bd853ed80210bc44bf2dfa86265055e1d1b6043
eggøer <eggercedric78@gmail.com>
2020-09-19 16:10:24 +0200

    feat(home): Add Css, title & input text search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
+  title = 'Search Engine';
+  
   constructor() { }
 
   ngOnInit(): void {
